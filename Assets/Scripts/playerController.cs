b129f783341ce657285ac25d96c723779ab6e247
RoniPTKT <fropi002@edu.xamk.fi>
2024-01-31 15:53:01 +0200

    Worked on driving and collision physics

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -29,23 +29,44 @@ public class playerController : MonoBehaviour
         accelerateInput = Input.GetAxis("Vertical");
         steerInput = Input.GetAxis("Horizontal");
 
-
+        //Check if accelerator is being pressed and if car is moving slower than maxSpeed
         if((accelerateInput != 0) && (currentSpeed <= maxSpeed))
         {
+            //Increase the car speed towards the pressed direction
             currentSpeed += accelerateInput * 0.01f;
         }
-        else if( currentSpeed < 0)
+        //Check if accelerator is pressed and if car is moving backwards
+        else if(accelerateInput == 0 && currentSpeed < 0)
         {
+            //Decrease the car speed (adding to negative speed)
             currentSpeed += 0.01f;
         }
-        else if (currentSpeed > 0)
+        //Check if accelerator is pressed and if car is moving forward
+        else if (accelerateInput == 0 && currentSpeed > 0)
         {
+            //Decrease car speed
             currentSpeed -= 0.01f;
         }
-        currentSteer = steerInput * 100;
-        transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed);
-        if(currentSpeed != 0)
+        //Check if accelerator is pressed and if car is slow enough to stop
+        if (accelerateInput == 0 && currentSpeed < 0.01f && currentSpeed > 0.01f)
         {
+            //Stop the car
+            currentSpeed = 0;
+        }
+        //Move the car with the current speed value
+        transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed);
+
+        //Make the current steering angle relative to the input and current speed
+        currentSteer = steerInput * currentSpeed * 10 - (currentSpeed * 0.9f);
+
+        if (steerInput == 0)
+        {
+            currentSteer = 0;
+        }
+        //Check if car is moving
+        if (currentSpeed > 1 || currentSpeed < -1)
+        {
+            //Turn the car towards the steer direction
             transform.Rotate(Vector3.up * Time.deltaTime * currentSteer);
         }
         if (currentSpeed > 0 && accelerateInput < 0)
@@ -53,13 +74,16 @@ public class playerController : MonoBehaviour
             currentSpeed -= 0.1f;
         }
 
+        //Check if car is on top of a collider
         if (Physics.Raycast(transform.position, Vector3.down, 0.5f))
         {
+            //Íf yes, car won't fall
             grounded = true;
             fallSpeed = 0;
         }
         else
         {
+            //If no, car starts to fall with an increasing speed
             grounded = false;
             fallSpeed += gravity;
             transform.Translate(Vector3.down * Time.deltaTime * fallSpeed, Space.World);
