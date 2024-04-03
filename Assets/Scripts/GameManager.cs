62e8de88e9e0601ee9f54b8b77d201714031301f
RoniS02 <erosu002@edu.xamk.fi>
2024-04-03 14:13:57 +0300

    UI pause menu
    made pause menu to sorta work

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Boolean isGamePaused;
+    public GameObject PauseMenu;
+    public void OnPlay()
     {
-        
+        SceneManager.LoadScene("main_testscene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnQuit()
     {
-        
+        Application.Quit();
+    }
+
+    public void pauseGame()
+    {
+        PauseMenu.gameObject.SetActive(true);
+        Time.timeScale = 0;
+        isGamePaused = true;
+        GetComponent<playerController>();
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void continueGame()
+    {
+        PauseMenu.gameObject.SetActive(false);
+        Time.timeScale = 1;
+        isGamePaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+           if(isGamePaused)
+            {
+                continueGame();
+                Debug.Log("Game should not be paused");
+            } else
+            {
+                pauseGame();
+                Debug.Log("Game paused");
+            }
+        }
     }
 }
