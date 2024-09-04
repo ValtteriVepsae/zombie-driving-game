a86504fa91df75bc19ed7fbc15218d684275c0d5
RoniPTKT <fropi002@edu.xamk.fi>
2024-09-04 17:02:26 +0300

    Reworked car handling

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] GameObject tire_fl;
+    [SerializeField] GameObject tire_fr;
+    [SerializeField] GameObject tire_bl;
+    [SerializeField] GameObject tire_br;
+    [SerializeField] WheelCollider tire_fl_col;
+    [SerializeField] WheelCollider tire_fr_col;
+    [SerializeField] WheelCollider tire_bl_col;
+    [SerializeField] WheelCollider tire_br_col;
+
+
+    public float accelerateInput;
+    public float steerInput;
+    int reverseGear;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        tire_fl_col = tire_fl.GetComponentInChildren<WheelCollider>();
+        tire_fr_col = tire_fr.GetComponentInChildren<WheelCollider>();
+        tire_bl_col = tire_bl.GetComponentInChildren<WheelCollider>();
+        tire_br_col = tire_br.GetComponentInChildren<WheelCollider>();
+        */
+        tire_fl_col.brakeTorque = 0;
+        tire_fr_col.brakeTorque = 0;
+        tire_br_col.brakeTorque = 0;
+        tire_bl_col.brakeTorque = 0;
+        reverseGear = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        accelerateInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Horizontal");
+        if (accelerateInput > 0)
+        {
+            tire_fl_col.motorTorque = accelerateInput * Time.deltaTime * 100000 * -reverseGear;
+            tire_fr_col.motorTorque = accelerateInput * Time.deltaTime * 100000 * -reverseGear;
+            tire_fl_col.brakeTorque = 0;
+            tire_fr_col.brakeTorque = 0;
+        }
+        if (accelerateInput < 0)
+        {
+            tire_fl_col.motorTorque = 0;
+            tire_fr_col.motorTorque = 0;
+            tire_fl_col.brakeTorque = -accelerateInput * Time.deltaTime * 10000;
+            tire_fr_col.brakeTorque = -accelerateInput * Time.deltaTime * 10000;
+        }
+
+        if (steerInput != 0)
+        {
+            tire_fl_col.steerAngle = steerInput *10;
+            tire_fr_col.steerAngle = steerInput * 10;
+
+        }
+        
+        if(Input.GetKeyDown(KeyCode.R) && reverseGear == -1)
+        {
+            reverseGear = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            reverseGear = -1;
+        }
+        
+}
+    }
